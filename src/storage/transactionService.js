d3fce2445a3c416e092095eaa6d659fcc601eee4
Marcos Montes <marcmontes@gmail.com>
2025-04-16 16:27:03 +0200

    transactionService.js

---
+++ src/storage/transactionService.js
@@ -0,0 +1,21 @@
+const Transaction = require('./transactionModel');
+
+exports.saveTransaction = async function (tx) {
+  const transaction = new Transaction(tx);
+  return await transaction.save();
+};
+
+exports.getAllTransactions = async function () {
+  return await Transaction.find();
+};
+
+exports.getTransactionById = async function (id) {
+  return await Transaction.findOne({ paymentId: id });
+};
+
+exports.updateTransactionStatus = async function (id, newStatus) {
+  return await Transaction.updateOne(
+    { paymentId: id },
+    { status: newStatus, updatedAt: new Date() }
+  );
+};
