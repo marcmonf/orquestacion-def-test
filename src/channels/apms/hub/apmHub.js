f1bf2468fb4348c0fdb94566c3ad47967e48bbbf
Marcos Montes <marcmontes@gmail.com>
2025-04-17 16:02:12 +0200

    Update apmHub.js

--- src/channels/apms/hub/apmHub.js
+++ src/channels/apms/hub/apmHub.js
@@ -1,13 +1,21 @@
-const bizum = require('./connectors/bizumConnector');
-// Aquí en el futuro irás añadiendo más conectores:
-// const blik = require('./connectors/blikConnector');
+const bizum = require('../connectors/bizumConnector');
+const blik = require('../connectors/blikConnector');
+// Puedes añadir más APMs aquí:
+// const twint = require('../connectors/twintConnector');
+// const mbway = require('../connectors/mbwayConnector');
 
 const apmHub = async (tx) => {
   switch (tx.method) {
     case 'bizum':
       return await bizum.process(tx);
-    // case 'blik':
-    //   return await blik.process(tx);
+
+    case 'blik':
+      return await blik.process(tx);
+
+    // Añadir más casos según APMs disponibles:
+    // case 'twint':
+    //   return await twint.process(tx);
+
     default:
       throw new Error(`APM no soportado: ${tx.method}`);
   }
