d65e5bd179261abdcc8b1167621bcf8da00eaaa2
Marcos Montes <marcmontes@gmail.com>
2025-04-18 13:24:46 +0200

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,17 @@
+// src/utils/logger.js
+const { createLogger, transports, format } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console()
+  ]
+});
+
+module.exports = logger;
