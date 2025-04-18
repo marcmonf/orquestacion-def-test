d2d870181d483d317d345aa927192f3b479e07c7
Marcos Montes <marcmontes@gmail.com>
2025-04-18 17:33:05 +0200

    Update transactionValidator.js

--- src/validators/transactionValidator.js
+++ src/validators/transactionValidator.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const transactionSchema = Joi.object({
-  paymentId: Joi.string().optional(),
+  paymentId: Joi.string().required(),
   amount: Joi.number().positive().required(),
   currency: Joi.string().length(3).required(),
   method: Joi.string().required(), // Ej: card, bizum, blik, etc.
