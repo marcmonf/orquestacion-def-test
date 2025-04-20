905afa670a101ef5a52a677bfb271712b3769fc4
Marcos Montes <marcmontes@gmail.com>
2025-04-20 09:25:00 +0200

    Create rateLimiterWebhooks.js

---
+++ src/middleware/rateLimiterWebhooks.js
@@ -0,0 +1,19 @@
+// src/middleware/rateLimiterWebhooks.js
+const rateLimit = require('express-rate-limit');
+const logger = require('../utils/logger');
+
+const rateLimiterWebhooks = rateLimit({
+  windowMs: 60 * 1000, // 1 minuto
+  max: 100,            // Hasta 100 peticiones por minuto por IP
+  message: {
+    error: 'Demasiadas peticiones entrantes al endpoint de webhooks. Intenta más tarde.'
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res, next, options) => {
+    logger.warn(`Rate limit en /webhooks desde IP ${req.ip}`);
+    res.status(options.statusCode).json(options.message);
+  }
+});
+
+module.exports = rateLimiterWebhooks;
