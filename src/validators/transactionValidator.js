43f02749473da89ad2c16ffc39164177c9bb4372
Marcos Montes <marcmontes@gmail.com>
2025-04-20 16:38:22 +0200

    Update transactionValidator.js

--- src/validators/transactionValidator.js
+++ src/validators/transactionValidator.js
@@ -8,7 +8,10 @@ const transactionSchema = Joi.object({
   status: Joi.string().valid('approved', 'declined', 'pending').required(),
   merchantId: Joi.string().required(),
   userId: Joi.string().optional(),
-  reference: Joi.string().optional()
+  reference: Joi.string().optional(),
+  cardholderName: Joi.string().min(2).required(),
+  expiryMonth: Joi.string().pattern(/^(0[1-9]|1[0-2])$/).required(), // 01 a 12
+  expiryYear: Joi.string().pattern(/^\d{4}$/).required() // 4 dígitos
 });
 
 module.exports = transactionSchema;
