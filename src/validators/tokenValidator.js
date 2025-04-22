a7e282e65edbf530a4a957def0ae3beb3f09d6e5
Marcos Montes <marcmontes@gmail.com>
2025-04-22 22:00:42 +0100

    Update tokenValidator.js

--- src/validators/tokenValidator.js
+++ src/validators/tokenValidator.js
@@ -1,33 +1,55 @@
-// src/validators/tokenValidator.js
 const Joi = require('joi');
 
 const currentYear = new Date().getFullYear();
 
 const tokenSchema = Joi.object({
-  cardNumber: Joi.string().creditCard().required(),
+  cardNumber: Joi.string()
+    .creditCard()
+    .required()
+    .messages({
+      'string.creditCard': 'token.invalid.cardNumber',
+      'any.required': 'token.invalid.cardNumber'
+    }),
 
   expiryMonth: Joi.string()
     .pattern(/^(0[1-9]|1[0-2])$/)
-    .required(),
+    .required()
+    .messages({
+      'string.pattern.base': 'token.invalid.expiryMonth',
+      'any.required': 'token.invalid.expiryMonth'
+    }),
 
   expiryYear: Joi.string()
     .pattern(/^\d{4}$/)
+    .required()
     .custom((value, helpers) => {
       if (parseInt(value) < currentYear) {
-        return helpers.error('expiryYear.tooEarly', { currentYear });
+        return helpers.error('token.invalid.expiryYear.tooLow');
       }
       return value;
     })
-    .required(),
+    .messages({
+      'string.pattern.base': 'token.invalid.expiryYear',
+      'any.required': 'token.invalid.expiryYear'
+    }),
 
   cvv: Joi.string()
     .pattern(/^\d{3,4}$/)
-    .required(),
+    .required()
+    .messages({
+      'string.pattern.base': 'token.invalid.cvv',
+      'any.required': 'token.invalid.cvv'
+    }),
 
   cardholderName: Joi.string()
     .min(2)
     .max(64)
     .required()
+    .messages({
+      'string.min': 'token.invalid.cardholderName.tooShort',
+      'string.max': 'token.invalid.cardholderName.tooLong',
+      'any.required': 'token.invalid.cardholderName.required'
+    })
 });
 
 module.exports = tokenSchema;
