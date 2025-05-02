d8c534251251a9e6a996ae8170a1d38ca0a0539f
Marcos Montes <marcmontes@gmail.com>
2025-05-02 19:55:58 +0200

    Update Token.js

--- src/models/Token.js
+++ src/models/Token.js
@@ -1,11 +1,14 @@
+// src/models/Token.js
 const mongoose = require('mongoose');
 
 const tokenSchema = new mongoose.Schema({
-  token:         { type: String, required: true, unique: true },
-  pan:           { type: String, required: true },
-  expiryMonth:   { type: String, required: true },
-  expiryYear:    { type: String, required: true },
-  cardholderName:{ type: String, required: true }
+  token:          { type: String, required: true, unique: true },
+  pan:            { type: String, required: true }, // cifrado
+  bin:            { type: String, required: true },
+  last4:          { type: String, required: true },
+  expiryMonth:    { type: String, required: true },
+  expiryYear:     { type: String, required: true },
+  cardholderName: { type: String, required: true }
 }, {
   timestamps: true // Crea automáticamente createdAt y updatedAt
 });
