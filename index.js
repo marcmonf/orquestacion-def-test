26133130c0eb87d1f1a19cb2ff25297f876e5993
Marcos Montes <marcmontes@gmail.com>
2025-05-06 22:58:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -58,11 +58,12 @@ app.use(hpp());
 app.use(i18nMiddleware);
 
 // Rutas protegidas
-app.use('/apms',         validateApiKey,       rateLimiter,         require('./src/channels/apms/apmsHandler'));
-app.use('/transactions', validateApiKey,       rateLimiter,         require('./src/routes/transactions'));
-app.use('/tokens',       validateTokenApiKey,  rateLimiterTokens,   require('./src/tokens/tokenRoutes')); // ✅ cambiado
-app.use('/analytics',    validateApiKey,       rateLimiter,         require('./src/routes/analytics'));
-app.use('/merchants',    validateApiKey,       rateLimiter,         require('./src/routes/merchantRoutes'));
+app.use('/apms',             validateApiKey,       rateLimiter,         require('./src/channels/apms/apmsHandler'));
+app.use('/transactions',     validateApiKey,       rateLimiter,         require('./src/routes/transactions'));
+app.use('/tokens',           validateTokenApiKey,  rateLimiterTokens,   require('./src/tokens/tokenRoutes'));
+app.use('/analytics',        validateApiKey,       rateLimiter,         require('./src/routes/analytics'));
+app.use('/merchants',        validateApiKey,       rateLimiter,         require('./src/routes/merchantRoutes'));
+app.use('/recurrent-profiles', validateApiKey,     rateLimiter,         require('./src/routes/recurrentProfiles')); // ✅ NUEVA RUTA
 
 // Ruta de prueba
 app.use('/test', require('./src/routes/testRoutes'));
