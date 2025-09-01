719f7ff10f897028df5a7720354e12589a1d3b88
MarcMonF <41896574+marcmonf@users.noreply.github.com>
2025-09-01 22:23:28 +0200

    Update rulesRoutes.js

--- src/routes/rulesRoutes.js
+++ src/routes/rulesRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const adminAuth = require('../middleware/adminAuth');
-const { getPolicy, validatePolicy, upsertPolicy } = require('../controllers/rulesController');
+const { getPolicy, validatePolicy, upsertPolicy, tryPolicy, getAudit } = require('../controllers/rulesController');
 
 // GET /rules/:merchantId
 router.get('/:merchantId', adminAuth, getPolicy);
@@ -12,5 +12,11 @@ router.post('/validate', adminAuth, validatePolicy);
 
 // PUT /rules/:merchantId
 router.put('/:merchantId', adminAuth, upsertPolicy);
+
+// POST /rules/try
+router.post('/try', adminAuth, tryPolicy);
+
+// GET /rules/:merchantId/audit
+router.get('/:merchantId/audit', adminAuth, getAudit);
 
 module.exports = router;
